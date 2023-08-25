20738e3ea003bb2e6b131e72aaf6761020a0ee6f
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2023-08-25 09:05:46 -0500

    Add files via upload

--- fillTreasure.java
+++ fillTreasure.java
@@ -1,7 +1,47 @@
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.JFrame.*;
+import java.awt.Graphics;
 public class fillTreasure {
     private int zeros,ones,twos,threes;
-    private int fours, fives, siexes
+    private int fours, fives, sixes, sevens;
+    private int eights, nines, tens, elevens;
+    private int twelves, thirteens, fourteens, fifteens;
     public fillTreasure(){
+        zeros = 2;
+        ones = 2;
+        twos = 2;
+        threes = 2;
+        fours = 2;
+        fives = 2;
+        sixes = 2;
+        sevens = 2;
+        eights = 2;
+        nines = 2;
+        tens = 2;
+        elevens = 2;
+        twelves =2;
+        thirteens = 2;
+        fourteens = 2;
+        fifteens = 2;
+    }
+    public ArrayList fill(ArrayList<Treasure> t){
+        ArrayList<Treasure> answer = fillTier1(t);
+        answer.add()
+        return answer;
+    }
+    public ArrayList fillTier1(ArrayList<Treasure> t){
+
+    }
+    public ArrayList fillTier2(){
+
+    }
+    public ArrayList fillTier3(){
+
+    }
+    public ArrayList fillTier4(){
 
     }
 }
